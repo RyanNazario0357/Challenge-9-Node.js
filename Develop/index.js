df2e9d6c6a56e40d1c6c0ab9c528214329fc635b
RyanNazario0357 <Ryan0357@icloud.com>
2022-08-11 19:19:41 -0400

    added description,installation, and usage section

--- Develop/index.js
+++ Develop/index.js
@@ -19,9 +19,48 @@ const questions = [
                 return false;
             }
             }
-        }
-];
+        },
 
+    {
+        type: 'input',
+        name: 'Description',
+        message: 'What is your Description',
+        validate: your_Description => {
+            if (your_Description) {
+                return true;
+            } else {
+                console.log('Please enter a Description.');
+                return false;
+            }
+            }
+    },
+
+    {
+        type: 'input',
+        name: 'Installation',
+        message: 'How can I install your project?',
+        validate: your_Installation => {
+            if (your_Installation) {
+                return true;
+            } else {
+                console.log('Please enter the steps on how to install it.');
+                return false;
+            }
+            }
+        },
+
+        {
+        type: 'input',
+        name: 'Usage',
+        message: 'How can I use it?',
+        validate: your_Usage => {
+            if (your_Usage) {
+                return true;
+            } else {
+                console.log('Please explain how i can use it.');
+                return false;
+            }
+            },
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
