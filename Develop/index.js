bcba05576a502caa43e9f49aa9efddf79cdae916
RyanNazario0357 <Ryan0357@icloud.com>
2022-08-11 19:04:22 -0400

    added insert title section

--- Develop/index.js
+++ Develop/index.js
@@ -5,7 +5,22 @@ const generateMarkdown = require('./generateMarkdown')
 console.log("Hello User. This is ReadMe!")
 console.log("Please answer a couple of the questions below so that i can create a ReadMe file.")
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+
+    {
+        type: 'input',
+        name: 'Title',
+        message: 'What is your title',
+        validate: your_Input => {
+            if (your_Input) {
+                return true;
+            } else {
+                console.log('Please enter a title.');
+                return false;
+            }
+            }
+        }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
