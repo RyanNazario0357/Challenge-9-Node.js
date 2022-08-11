1bedd6168119c10a28a9946d35f79c53bab2788d
RyanNazario0357 <Ryan0357@icloud.com>
2022-08-11 18:28:56 -0400

    removed previous code and typed new code

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,15 +1,9 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  let licenseBadgeOutput = ""
-  let licenseLink = []
-  for(var 1 = 0; i<license.license.length;i++)
-  {
-    if(i == license.license.length-1)
-    comma = ""
-    
-  }
+  return '# ${data.License}'
 }
+
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
