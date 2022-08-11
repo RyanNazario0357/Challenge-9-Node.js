cf6b8539ec5f0eb38b42d4a9cdf5ee2300286c94
RyanNazario0357 <Ryan0357@icloud.com>
2022-08-11 18:20:39 -0400

    added license render code

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,15 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  let licenseBadgeOutput = ""
+  let licenseLink = []
+  for(var 1 = 0; i<license.license.length;i++)
+  {
+    if(i == license.license.length-1)
+    comma = ""
+    
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
